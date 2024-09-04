4bd7956b29583b66c1c49334a70e189172b33aec
Ronsen <ronsen@startrek.or.id>
2024-09-04 14:22:06 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,10 +20,6 @@ class AppServiceProvider extends ServiceProvider
 	 */
 	public function boot(): void
 	{
-		if (app()->isProduction()) {
-			$this->app['request']->server->set('HTTPS', true);
-		}
-		
 		Paginator::useTailwind();
 	}
 }
