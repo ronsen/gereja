e70b50f699aa2370f52758952187ef1d6e86d2f6
Ronsen <ronsen@startrek.or.id>
2024-09-04 14:24:15 +0700

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\TrustProxies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,9 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->web(append: [
-			TrustProxies::class,
-		]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
